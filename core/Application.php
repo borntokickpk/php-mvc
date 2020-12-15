e008ef7973caa699358c0259dc108e5e5af1b42d
Hassan <hassanhafiz44@gmail.com>
2020-12-15 21:19:07 +0500

    Setting up router and request in Application

--- core/Application.php
+++ core/Application.php
@@ -4,5 +4,12 @@ namespace app\core;
 
 class Application
 {
+    public Router $router;
+    public Request $request;
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+    }
 
 }
