4dd8e719c30d1f43ff5fe431d14baf7be19ddb31
Hassan <hassanhafiz44@gmail.com>
2020-12-15 21:03:59 +0500

    getPath added to get current path

--- core/Request.php
+++ core/Request.php
@@ -4,5 +4,15 @@ namespace app\core;
 
 class Request
 {
+    public function getPath()
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
 
+        if($position === false) {
+            return $path;
+        }
+
+        return substr($path, 0, $position);
+    }
 }
