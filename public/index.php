e418d3e6128c9b30403b0e3fe9477f8b0049f79e
Hassan <hassanhafiz44@gmail.com>
2020-12-16 00:23:36 +0800

    Added closure routes

--- public/index.php
+++ public/index.php
@@ -6,4 +6,12 @@ use app\core\Application;
 
 $app = new Application;
 
+$app->router->get('/', function() {
+    return "Hello world...";
+});
+
+$app->router->get('/contact', function() {
+    return "Contact us...";
+});
+
 $app->run();
