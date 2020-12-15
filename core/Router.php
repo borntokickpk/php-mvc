b4afc1b400e05c933361867bcd0136253f8ace95
Hassan <hassanhafiz44@gmail.com>
2020-12-15 21:16:57 +0500

    Router::get and Router::post implemented - saving routes and callback

--- core/Router.php
+++ core/Router.php
@@ -12,4 +12,13 @@ class Router
         $this->request = $request;
     }
 
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
 }
