f13536378216328c1a412d07f6b4622bfa5fba31
Hassan <hassanhafiz44@gmail.com>
2020-12-16 23:35:48 +0500

    Render home from SiteController::home with params

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,7 +2,17 @@
 
 namespace app\controllers;
 
+use app\core\Application;
+
 class SiteController
 {
-    
+    public function home()
+    {
+        $params = [
+            "name" => "Hassan Javed",
+        ];
+
+        return Application::$app->router->renderView('home', $params);
+    }
+
 }
--- views/home.php
+++ views/home.php
@@ -1,1 +1,3 @@
 <h1>Home</h1>
+
+<h2>Welcome <?php echo $name ?></h2>
