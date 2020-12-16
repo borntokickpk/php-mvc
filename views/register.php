bbeeb89b847a461b2cc03684ee29fbc02780e1eb
Hassan <hassanhafiz44@gmail.com>
2020-12-17 00:44:48 +0500

    Added app\controllers\AuthController - renders login and register form

---
+++ views/register.php
@@ -0,0 +1,32 @@
+<h1>Register</h1>
+
+<form action="" method="POST">
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="firstname">Firstname</label>
+                <input type="text" class="form-control" id="firstname" name="firstname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label for="lastname">Lastname</label>
+                <input type="text" class="form-control" id="lastname" name="lastname">
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <div class="form-group">
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="pass" class="form-control" id="confirmpassword" name="confirmPassword">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
