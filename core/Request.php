cf9f206c425eef2431f4edee05b47b2bc8b3cf9d
Hassan <hassanhafiz44@gmail.com>
2020-12-17 00:03:37 +0500

    Added app\core\Request::getBody to get - Sanitized input from the forms

--- core/Request.php
+++ core/Request.php
@@ -30,4 +30,23 @@ class Request
     {
         return $this->getMethod() === 'post';
     }
+
+    public function getBody()
+    {
+        $body = [];
+        
+        if($this->isGet()) {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        
+        if($this->isPOST()) {
+            foreach ($_POST as $key => $value) {
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        return $body;
+    }
 }
