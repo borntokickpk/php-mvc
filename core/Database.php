17e8768a8d6f09132c12fa40c4b43045990d7b82
Hassan <hassanhafiz44@gmail.com>
2020-12-18 03:25:15 +0800

    apply migration function added

--- core/Database.php
+++ core/Database.php
@@ -18,6 +18,34 @@ class Database
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigrations();
+
+        $newMigrations = [];
+        $files = scandir(Application::$ROOT_PATH."/migrations");
+        $migrationsToApply = array_diff($files, $appliedMigrations);
+
+        foreach ($migrationsToApply as $migration) {
+            if($migration === "." || $migration === "..") continue;
+
+            require_once Application::$ROOT_PATH . "/migrations/$migration";
+            $className = pathinfo($migration, PATHINFO_FILENAME);
+            $instance = new $className();
+
+            $this->log("Applying migration $migration");
+            $instance->up();
+            $this->log("Applied migration $migration");
+            $newMigrations[] = $migration;
+        }
+
+        if(!empty($newMigrations)) {
+            $this->saveMigrations($newMigrations);
+        } else {
+            $this->log("All migrations applied");
+        }
+    }
 
     public function createMigrationsTable()
     {
