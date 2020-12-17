f7c8367d35332b4710c5f7d78376b47cd36df1b1
Hassan <hassanhafiz44@gmail.com>
2020-12-18 03:26:51 +0800

    Application now has db instance

--- core/Application.php
+++ core/Application.php
@@ -10,13 +10,16 @@ class Application
     public static string $ROOT_PATH;
     public static Application $app;
     public Controller $controller;
-    public function __construct($rootPath)
+    public Database $db;
+    public function __construct($rootPath, array $config)
     {
         self::$ROOT_PATH = $rootPath;
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
     }
 
     public function run()
